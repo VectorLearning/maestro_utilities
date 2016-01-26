f3376c1b80355ea65097d2ac0349e1f1df4edbbc
Jason Paluck <jason@centrelearn.com>
2016-01-26 14:29:14 -0500

    Raise error if MAESTRO_SEED variable not set

--- lib/maestro_utilities.rb
+++ lib/maestro_utilities.rb
@@ -1,5 +1,7 @@
+require "maestro_utilities/errors"
 require "maestro_utilities/version"
 require 'maestro_utilities/signature'
 
 module MaestroUtilities
+  raise ConfigurationError, "No MAESTRO_SEED environment variable found!" unless ENV['MAESTRO_SEED']
 end
---
+++ lib/maestro_utilities/errors.rb
@@ -0,0 +1,4 @@
+module MaestroUtilities
+  Error = Class.new(StandardError)
+  ConfigurationError = Class.new(Error)
+end
