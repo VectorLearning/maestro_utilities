4b31dbdbd87f22b96e7e1dfaa2fe163c8047317b
Alex Spencer <alex@alexspencer.net>
2016-04-12 16:02:38 -0400

    accept sub-topics on get_competency

--- lib/maestro/data/competency_topic.rb
+++ lib/maestro/data/competency_topic.rb
@@ -1,8 +1,9 @@
 module Maestro
   module Data
     class CompetencyTopic < Model
-      attribute :id,    Integer
-      attribute :name,  String
+      attribute :id,         Integer
+      attribute :name,       String
+      attribute :sub_topics, Array[self]
     end
   end
 end
