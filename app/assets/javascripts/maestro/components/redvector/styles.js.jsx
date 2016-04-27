6bb0f0d4156981e74771440564d7400735771452
Peter Kraus <fuudstamps@gmail.com>
2016-04-27 10:30:43 -0400

    finish navbar styles

--- app/assets/javascripts/maestro/components/redvector/styles.js.jsx
+++ app/assets/javascripts/maestro/components/redvector/styles.js.jsx
@@ -5,7 +5,8 @@ var Styles = React.createClass({
     styleCode += ".navbar .nav a:hover, .navbar .nav a:focus, .navbar .nav li.dropdown .dropdown-menu li.active a { background-color: " + this.props.colors.hover_background +"\!important; color: " + this.props.colors.hover_text +"\!important; }";
     styleCode += ".navbar .nav li { border: 0 !important\; }";
     styleCode += ".navbar .navbar-toggle:hover, .navbar .navbar-toggle:focus { background-color: " + this.props.colors.hover_background + "!important\; }";
-    styleCode += ".navbar .navbar-toggle:hover .text, .navbar .navbar-toggle:focus .text { color: " + this.props.colors.hover_text + "\; }";
+    styleCode += ".navbar .navbar-toggle .text, .navbar .navbar-toggle i.fa { color: " + this.props.colors.text + "\; }";
+    styleCode += ".navbar .navbar-toggle:hover .text, .navbar .navbar-toggle:focus .text, .navbar .navbar-toggle:hover i.fa, .navbar .navbar-toggle:focus i.fa { color: " + this.props.colors.hover_text + "\; }";
     return(
       <style>{styleCode}</style>
     );
