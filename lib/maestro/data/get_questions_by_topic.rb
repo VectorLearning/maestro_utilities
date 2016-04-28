1f01bf9228057c66a2bff22e9b7415865754e83e
Alex Spencer <alex@alexspencer.net>
2016-04-28 10:06:59 -0400

    Update adapter to work with cached questions

--- lib/maestro/data/get_questions_by_topic.rb
+++ lib/maestro/data/get_questions_by_topic.rb
@@ -8,20 +8,17 @@ module Maestro
 
       extend HttpService
 
-      def self.call(session, topic_id)
+      def self.call(session, topic_ids)
         response = Maestro.connection.get do |request|
-          request.url "/v1/lms/competency_topics/#{topic_id}/questions"
+          request.url "/v1/lms/questions"
           request.params['token'] = session.is_a?(String) ? session : session.token
+          request.params['topic_ids'] = topic_ids
         end
 
         ensure_successful_response(response)
 
         data = parse_json(response.body)
-        questions = data.fetch('questions', [])
-          .map { |data| ::Maestro::Data::Question.new(data) }
-        topic = ::Maestro::Data::CompetencyTopic.new(data['topic'])
-
-        Response.new(questions: questions, topic: topic)
+        data
       end
     end
   end
