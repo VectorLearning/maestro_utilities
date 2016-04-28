df73faaea3be3df57ab7c98dadeaab4abc4319d0
Alex Spencer <alex@alexspencer.net>
2016-04-28 10:47:14 -0400

    Fix broken specs

--- lib/maestro/data/get_questions_by_topic.rb
+++ lib/maestro/data/get_questions_by_topic.rb
@@ -1,11 +1,6 @@
 module Maestro
   module Data
     module GetQuestionsByTopic
-      class Response < Model
-        attribute :questions, Array[::Maestro::Data::Question]
-        attribute :topic, ::Maestro::Data::CompetencyTopic
-      end
-
       extend HttpService
 
       def self.call(session, topic_ids)
