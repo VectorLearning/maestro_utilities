b4ee0572f8cc065805740e5e56ff808f694cbd3b
joshdsantos <joshdsantos@gmail.com>
2016-07-28 15:45:32 -0700

    Add ts sidenav

--- app/assets/javascripts/maestro/components/targetsolutions/components.js
+++ app/assets/javascripts/maestro/components/targetsolutions/components.js
@@ -1,1 +1,2 @@
 //= require ./navbar
+//= require ./sidenav
---
+++ app/assets/javascripts/maestro/components/targetsolutions/sidenav.js.jsx
@@ -0,0 +1,28 @@
+//= require react
+//= require react_ujs
+
+var SideNav = React.createClass({
+  render: function() {
+    var links = [
+      {name: "Home", icon: "home", active: true},
+      {name: "Home", icon: "home"},
+      {name: "Home", icon: "home"}
+    ];
+    var navItems = links.map(function(link){
+      return <NavItem name={ link.name } icon={ link.icon } active={link.active} />
+    });
+    return (
+      <ul className="nav nav-pills nav-stacked" id="sidenav">
+        { navItems }
+      </ul>
+    );
+  }
+});
+
+var NavItem = React.createClass({
+  render: function() {
+    return (
+      <li className={(this.props.active ? "active" : "")}><a href=""><i className={"fa fa-" + this.props.icon + " fa-2x"}></i><span>{ this.props.name }</span></a></li>
+    );
+  }
+});
