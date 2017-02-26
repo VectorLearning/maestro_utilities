a449c746aea030f358311eadb5f63e53ff1866e7
Josh Santos <joshdsantos@gmail.com>
2017-02-26 10:05:13 -0800

    Add NavBarNotifications

--- app/assets/javascripts/maestro/components/targetsolutions/components.js
+++ app/assets/javascripts/maestro/components/targetsolutions/components.js
@@ -1,6 +1,7 @@
 //= require ./navbar/NavBar
 //= require ./navbar/NavBarDropdown
 //= require ./navbar/NavBarLink
+//= require ./navbar/NavBarNotifications
 //= require ./sidenav/SideNav
 //= require ./sidenav/SideNavLink
 //= require ./sidenav/SideNavMenu
