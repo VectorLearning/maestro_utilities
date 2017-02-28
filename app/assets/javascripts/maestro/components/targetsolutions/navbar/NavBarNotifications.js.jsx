3e5ed4a00b947273434b245959d2f7b5604783fe
Josh Santos <joshdsantos@gmail.com>
2017-02-27 20:42:49 -0800

    Add popover POC

--- app/assets/javascripts/maestro/components/targetsolutions/navbar/NavBarNotifications.js.jsx
+++ app/assets/javascripts/maestro/components/targetsolutions/navbar/NavBarNotifications.js.jsx
@@ -4,30 +4,33 @@ class NavBarNotifications extends React.Component {
     this.classes = props.classes
   }
 
+  componentDidMount () {
+    $(this.refs.notificationsUser).popover({
+      container: 'body',
+      content: function () {
+        return '<ul className="list-group"><li class="list-group-item"><a href="#"><span class="titleSingle">You Have No Notifications</span></a></li><li class="list-group-item"><a href="/tsapp/dashboard/pl_fb/index.cfm?fuseaction=c_pro.showNotifications#/1372401/A2A90096CB2D32C08B841029BFA3DA83" title="See All My Notifications"><strong>See My Notifications</strong></a></li></ul>'
+      },
+      html: true,
+      placement: 'bottom',
+      title: 'My Notifications',
+      trigger: 'focus'
+    })
+  }
+
   render () {
     return (
       <ul className={`nav navbar-nav ${this.classes}`} id='navbar-notifications-menu'>
         <li>
-          <a id='navbar-notifications-user' href='#' tabindex='0' className='navbar-notifications'>
+          <a id='navbar-notifications-user' href='#' className='navbar-notifications' ref='notificationsUser'>
             <i className='fa fa-user fa-fw fa-lg' />
             <span className='badge'>0</span>
           </a>
-          <div className='hide' id='navbar-notifications-user-popover'>
-            <ul className='list-group'>
-              <li className='list-group-item'><img src='assets/img/loaders/loading.gif' /></li>
-            </ul>
-          </div>
         </li>
         <li>
-          <a id='navbar-notifications-admin' href='#' tabindex='0' className='navbar-notifications '>
+          <a id='navbar-notifications-admin' href='#' className='navbar-notifications '>
             <i className='fa fa-users fa-fw fa-lg' />
             <span className='badge'>0</span>
           </a>
-          <div className='hide' id='navbar-notifications-admin-popover'>
-            <ul className='list-group'>
-              <li className='list-group-item'><img src='assets/img/loaders/loading.gif' /></li>
-            </ul>
-          </div>
         </li>
       </ul>
     )
