cce15465c7621ae96c7bec89df82a1d1741e9fda
Scott Maslar <scott.maslar@targetsolutions.com>
2017-03-02 09:08:03 -0600

    Add support for header signatures
    We should be passing the non-request parameters via the headers but are
    not. We've updated maestro_utilities to support passing the token,
    signature, and expires via the headers, as well as updating the before
    filters to look for these values there.

--- app/controllers/concerns/maestro/controller_helper.rb
+++ app/controllers/concerns/maestro/controller_helper.rb
@@ -7,22 +7,14 @@ module Maestro
 
     private
 
-    def check_signature?
-      signature_params.key?(:expires) &&
-        signature_params.key?(:signature) &&
-        signature_params.key?(:token) &&
-        signature_params[:token] != session[:maestro_token]
-    end
-
-    def redirect_to_lms_on_session_expiration
-      unless maestro_session.valid?
-        lms_host = maestro_session.lms_data['lms_host']
-        redirect_to lms_host if lms_host
-      end
-    end
-
     def set_maestro_session
-      if check_signature?
+      if header_params_present?
+        if Maestro::Signature.valid?(request_params.merge(header_params))
+          session[:maestro_token] = header_params[:token]
+        else
+          instance_exec(&Maestro.config.after_invalid_signature)
+        end
+      else signature_params_present?
         if Maestro::Signature.valid?(signature_params)
           session[:maestro_token] = signature_params[:token]
         else
@@ -31,8 +23,39 @@ module Maestro
       end
     end
 
+    def header_params_present?
+      header_signature_params[:expires] &&
+      header_signature_params[:signature] &&
+      header_signature_params[:token] &&
+      header_signature_params[:token] != session[:maestro_token]
+    end
+
+    def header_params
+      @header_params ||= { token: request.headers['token'],
+                           signature: request.headers['signature'],
+                           expires: request.headers['expires'] }
+    end
+
+    def signature_params_present?
+      signature_params.key?(:expires) &&
+        signature_params.key?(:signature) &&
+        signature_params.key?(:token) &&
+        signature_params[:token] != session[:maestro_token]
+    end
+
     def signature_params
       @signature_params ||= request.query_parameters
+    end
+
+    def request_params
+      @request_params ||= request.query_parameters
+    end
+
+    def redirect_to_lms_on_session_expiration
+      unless maestro_session.valid?
+        lms_host = maestro_session.lms_data['lms_host']
+        redirect_to lms_host if lms_host
+      end
     end
   end
 end
