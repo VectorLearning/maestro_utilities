024fa002c7d9ce744da9c951a99a247f44403038
Michael Anhari <mpanhari@gmail.com>
2017-07-25 12:57:05 -0400

    Send more information when a user submits an issue

--- lib/maestro/data/create_user_issue.rb
+++ lib/maestro/data/create_user_issue.rb
@@ -3,14 +3,23 @@ module Maestro
     module CreateUserIssue
       extend HttpService
 
-      def self.call(session, course_id, text)
+      def self.call(session, course_run, issue)
         response = Maestro.connection.post do |request|
-          request.body = JSON.generate({
-            issue: { course_id: course_id, text: text },
-            token: session.token,
-          })
-          request.headers['Content-Type'] = 'application/json'
-          request.url('/v1/lms/profile/issues')
+          request.body = JSON.generate(
+            {
+              issue: {
+                course_id: course_run.course_id,
+                email: session[:lms_data][:email],
+                enrollment_id: course_run.enrollment_id,
+                text: issue[:text],
+                user_id: session[:lms_data][:user_id],
+                url: issue[:url],
+              },
+              token: session.token,
+            }
+          )
+          request.headers["Content-Type"] = "application/json"
+          request.url("/v1/lms/profile/issues")
         end
 
         ensure_successful_response(response)
