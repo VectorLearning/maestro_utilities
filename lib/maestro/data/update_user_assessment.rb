77cc7d3d2bf7cf7441f7d51ef82c13374ea9b6bb
MatthewNguyen-vt <matthew.nguyen@targetsolutions.com>
2017-12-20 19:29:50 -0800

    Modify UpdateUserAssessment service
    TargetSolutions completion API needs a course title.

--- lib/maestro/data/update_user_assessment.rb
+++ lib/maestro/data/update_user_assessment.rb
@@ -3,13 +3,14 @@ module Maestro
     module UpdateUserAssessment
       extend HttpService
 
-      def self.call(session, assessment_id, status, topic_results=[])
+      def self.call(session, assessment, status, topic_results=[])
         response = Maestro.connection.put do |request|
           request.body = JSON.generate(status: status,
                                        topic_results: topic_results,
-                                       token: session.token)
+                                       token: session.token,
+                                       title: assessment.title)
           request.headers['Content-Type'] = 'application/json'
-          request.url("/v1/lms/profile/assessments/#{assessment_id}")
+          request.url("/v1/lms/profile/assessments/#{assessment.id}")
         end
 
         ensure_successful_response(response)
