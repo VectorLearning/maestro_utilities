b2b80bff7daa31cc28219f90d94c67dfef9e0146
MatthewNguyen-vt <matthew.nguyen@targetsolutions.com>
2018-01-10 16:26:53 -0800

    Only call if completed
    For now, only call update assessment if completed for TS.  Will implement update later.

--- lib/maestro/data/update_user_assessment.rb
+++ lib/maestro/data/update_user_assessment.rb
@@ -4,13 +4,15 @@ module Maestro
       extend HttpService
 
       def self.call(session, assessment_id, status, topic_results=[], assessment_title)
-        response = Maestro.connection.put do |request|
-          request.body = JSON.generate(status: status,
-                                       topic_results: topic_results,
-                                       token: session.token,
-                                       title: assessment_title)
-          request.headers['Content-Type'] = 'application/json'
-          request.url("/v1/lms/profile/assessments/#{assessment_id}")
+        if status[:status] == 'completed'
+          response = Maestro.connection.put do |request|
+            request.body = JSON.generate(status: status,
+                                         topic_results: topic_results,
+                                         token: session.token,
+                                         title: assessment_title)
+            request.headers['Content-Type'] = 'application/json'
+            request.url("/v1/lms/profile/assessments/#{assessment_id}")
+          end
         end
 
         ensure_successful_response(response)
