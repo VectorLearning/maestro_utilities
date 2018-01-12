bfa4ccbad00954e585b132989c53d203de9f56dc
MatthewNguyen-vt <matthew.nguyen@targetsolutions.com>
2018-01-12 15:03:24 -0800

    Remove completion check in utils
    I meant for this check only for TargetSolutions.  Revert to the way it was before.

--- lib/maestro/data/update_user_assessment.rb
+++ lib/maestro/data/update_user_assessment.rb
@@ -4,18 +4,16 @@ module Maestro
       extend HttpService
 
       def self.call(session, assessment_id, status, topic_results=[], assessment_title)
-        if status[:status] == 'completed'
-          response = Maestro.connection.put do |request|
-            request.body = JSON.generate(status: status,
-                                         topic_results: topic_results,
-                                         token: session.token,
-                                         title: assessment_title)
-            request.headers['Content-Type'] = 'application/json'
-            request.url("/v1/lms/profile/assessments/#{assessment_id}")
-          end
-          
-          ensure_successful_response(response)
+        response = Maestro.connection.put do |request|
+          request.body = JSON.generate(status: status,
+                                       topic_results: topic_results,
+                                       token: session.token,
+                                       title: assessment_title)
+          request.headers['Content-Type'] = 'application/json'
+          request.url("/v1/lms/profile/assessments/#{assessment_id}")
         end
+        
+        ensure_successful_response(response)
       end
     end
   end
