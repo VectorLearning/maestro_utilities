e04cc0475b7e20b51688483f2224160e83cb78e0
MatthewNguyen-vt <matthew.nguyen@targetsolutions.com>
2018-01-17 18:57:59 -0800

    Update tests for get_ts_courses change

--- spec/maestro/data/get_ts_courses_spec.rb
+++ spec/maestro/data/get_ts_courses_spec.rb
@@ -4,13 +4,14 @@ module Maestro
   module Data
     RSpec.describe GetTsCourses do
       let!(:request) { stub_maestro_request(:get, path).with(body: json) }
-      let(:body)     { {start: startrow, length: limit, coursename: coursename, token: token} }
+      let(:body)     { {start: startrow, length: limit, coursename: coursename, coursetype: coursetype, token: token} }
       let(:json)     { JSON.generate(body) }
       let(:path)     { '/v1/lms/ts_courses' }
       let(:startrow)  { 0 }
       let(:limit) { 1000 }
       let(:coursename)  { 'Test Course' }
-      let(:response) { described_class.call(session, startrow, limit, coursename) }
+      let(:coursetype)  { 'TS Course' }
+      let(:response) { described_class.call(session, startrow, limit, coursename, coursetype) }
       let(:session)  { double('Session', token: token, user_id: user_id) }
       let(:user_id) { 1 }
       let(:token)    { 'token' }
