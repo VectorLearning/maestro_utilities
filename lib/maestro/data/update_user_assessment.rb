873ef3d2027b2fb3e1b510ff72979f22c0ef32fb
MatthewNguyen-vt <matthew.nguyen@targetsolutions.com>
2018-02-20 14:23:14 -0800

    Remove course title requirement for UpdateUserAssessment
    The LMS should be able to determine any assessment attribute from its id.
    
    # Conflicts:
    #	lib/maestro/data/update_user_assessment.rb

--- lib/maestro/data/update_user_assessment.rb
+++ lib/maestro/data/update_user_assessment.rb
@@ -3,12 +3,11 @@ module Maestro
     module UpdateUserAssessment
       extend HttpService
 
-      def self.call(session, assessment_id, status, topic_results=[], assessment_title="")
+      def self.call(session, assessment_id, status, topic_results=[])
         response = Maestro.connection.put do |request|
           request.body = JSON.generate(status: status,
                                        topic_results: topic_results,
-                                       token: session.token,
-                                       title: assessment_title)
+                                       token: session.token)
           request.headers['Content-Type'] = 'application/json'
           request.url("/v1/lms/profile/assessments/#{assessment_id}")
         end
