ff9e9db2a3192b4a6632c3809c55195513a1e27b
Peter Kraus <fuudstamps@gmail.com>
2018-02-21 15:57:20 -0500

    adds CBRE theme

---
+++ app/assets/javascripts/maestro/components/custom/cbre/NavLinkDropdown.js.jsx
@@ -0,0 +1,24 @@
+var NavLinkDropdown = React.createClass({
+  render: function(){
+    var active = false;
+    var links = this.props.links.map(function(link){
+      if(link.active){
+        active = true;
+      }
+      return (
+        <NavLink linkTo={link.link} text={link.text} active={link.active} />
+      );
+    });
+    return (
+      <li className={"dropdown " + (active ? "active" : "")}>
+        <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+          {this.props.text}
+          <span className="caret"></span>
+        </a>
+        <ul className="dropdown-menu">
+          {links}
+        </ul>
+      </li>
+    );
+  }
+});
---
+++ app/assets/javascripts/maestro/components/custom/cbre/components.js
@@ -0,0 +1,5 @@
+//= require ./NavBar
+//= require ./NavBrand
+//= require ./NavMenu
+//= require ./NavLinkDropdown
+//= require ./NavLink
