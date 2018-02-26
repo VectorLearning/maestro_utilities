bd5a32ed319d718994cbdf0b9056984e70e744a0
Michael Anhari <mpanhari@gmail.com>
2018-02-26 11:36:47 -0500

    Make assessment title optional

--- lib/maestro/data/update_user_assessment.rb
+++ lib/maestro/data/update_user_assessment.rb
@@ -12,7 +12,7 @@ module Maestro
           request.headers['Content-Type'] = 'application/json'
           request.url("/v1/lms/profile/assessments/#{assessment_id}")
         end
-        
+
         ensure_successful_response(response)
       end
     end
