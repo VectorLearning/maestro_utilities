5509f37b67a1814024b92bfc3d9fe87e4b8c6380
Michael Anhari <mpanhari@gmail.com>
2018-02-26 11:36:47 -0500

    Make assessment title optional

--- lib/maestro/data/update_user_assessment.rb
+++ lib/maestro/data/update_user_assessment.rb
@@ -3,7 +3,7 @@ module Maestro
     module UpdateUserAssessment
       extend HttpService
 
-      def self.call(session, assessment_id, status, topic_results=[], assessment_title)
+      def self.call(session, assessment_id, status, topic_results=[], assessment_title="")
         response = Maestro.connection.put do |request|
           request.body = JSON.generate(status: status,
                                        topic_results: topic_results,
@@ -12,7 +12,7 @@ module Maestro
           request.headers['Content-Type'] = 'application/json'
           request.url("/v1/lms/profile/assessments/#{assessment_id}")
         end
-        
+
         ensure_successful_response(response)
       end
     end
